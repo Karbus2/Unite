bea3ca951e75e9b2a2ef117e89fce5949c11a671
Karbus2 <122206606+Karbus2@users.noreply.github.com>
2024-04-25 18:40:00 +0200

    Initial Commit

---
+++ Unite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Unite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
