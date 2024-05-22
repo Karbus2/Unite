6bb42c1efdcdfa52cd545985d45280cb37548995
jan-maciejewski <jan.maciejewski@icloud.com>
2024-05-22 22:47:44 +0200

    register

--- Unite/Controllers/HomeController.cs
+++ Unite/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Unite.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
