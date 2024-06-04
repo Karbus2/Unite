f3feed26df955bbe79ca110fd682808ef5eb9b58
Karbus2 <122206606+Karbus2@users.noreply.github.com>
2024-06-04 21:27:14 +0200

    Simplified Events/Index and localized in PL Events/Index, Events/Details/

--- Unite/Models/Event.cs
+++ Unite/Models/Event.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using Unite.Models.CustomAttributes;
 
@@ -14,19 +15,29 @@ namespace Unite.Models
         }
 
         public Guid Id { get; set; }
+        [DisplayName("Tytuł")]
         public string Title { get; set; }
+        [DisplayName("Lokalizacja")]
         public string Location { get; set; }
+        [DisplayName("Zasięg")]
         public EventScope Scope { get; set; } = EventScope.FriendsOnly;
         [IsLower<DateTime>(nameof(End)), FutureDate]
+        [DisplayName("Data rozpoczęcia")]
         public DateTime Start { get; set; }
+        [DisplayName("Data zakończenia")]
         [IsHigher<DateTime>(nameof(Start)), FutureDate]
         public DateTime End { get; set; }
+        [DisplayName("ID Admina")]
         public Guid AdminId { get; set; }
+        [DisplayName("Admin")]
         public virtual ApplicationUser? Admin { get; set; }
         public virtual List<UserEvent>? Participants { get; set; }
+        [DisplayName("Opis")]
         public string? Description { get; set; }
+        [DisplayName("Maks. liczba uczestników")]
         [Range(2, int.MaxValue)]
         public int? Size { get; set; }
+        [DisplayName("Data utworzenia")]
         public DateTime CreatedDate { get; set; } 
         public DateTime UpdatedDate { get; set; } 
     }
