6b577f0f380252be9665cadcac0375f0d3ebcd8c
Karbus2 <122206606+Karbus2@users.noreply.github.com>
2024-06-04 21:17:02 +0200

    Removed Views Home/Index and Home/Privacy

--- Unite/Program.cs
+++ Unite/Program.cs
@@ -40,7 +40,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Events}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
