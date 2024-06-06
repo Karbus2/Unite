c3cb6b292f4a8a88d3b47ce00312dcfe8a2b6532
Karbus2 <122206606+Karbus2@users.noreply.github.com>
2024-06-06 19:42:42 +0200

    Authorization now required for HistoryEventsController

--- Unite/Controllers/HistoryEventsController.cs
+++ Unite/Controllers/HistoryEventsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,6 +12,7 @@ using Unite.Models;
 
 namespace Unite.Controllers
 {
+    [Authorize]
     public class HistoryEventsController : Controller
     {
         private readonly ApplicationDbContext _context;
